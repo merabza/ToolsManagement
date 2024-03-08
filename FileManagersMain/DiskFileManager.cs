4d66cecfcca2f74f105408d829d8802bc429c18e
merabza <merabza@gmail.com>
2024-03-08 14:44:13 +0400

    202403081443

--- FileManagersMain/DiskFileManager.cs
+++ FileManagersMain/DiskFileManager.cs
@@ -149,7 +149,7 @@ public sealed class DiskFileManager : FileManager
 
     public override string GetTextFileContent(string fileName)
     {
-        var fullFileName = Path.Combine(GetDirectoryInfo(null).Name, fileName);
+        var fullFileName = Path.Combine(GetDirectoryInfo(null).FullName, fileName);
         Logger.LogInformation("Get content from text file {fullFileName}", fullFileName);
         StreamReader reader = new(fullFileName);
         return reader.ReadToEnd();
