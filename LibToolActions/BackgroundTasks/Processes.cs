e1e36814c1605bba78b5b72dbae0bed0da68d733
merabza <merabza@gmail.com>
2024-04-12 17:37:38 +0400

    202404121736

--- LibToolActions/BackgroundTasks/Processes.cs
+++ LibToolActions/BackgroundTasks/Processes.cs
@@ -1,46 +1,48 @@
-﻿//using System.Threading.Tasks;
-//using Microsoft.Extensions.Logging;
+﻿using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 
-//namespace LibToolActions.BackgroundTasks;
+namespace LibToolActions.BackgroundTasks;
 
-//public sealed class Processes : IProcesses
-//{
-//    private readonly ILogger _logger;
+//ეს კლასი გამოიყენება ApAgent-ში
+public sealed class Processes : IProcesses
+{
+    private readonly ILogger _logger;
 
-//    private ProcessManager? _processManager;
+    private ProcessManager? _processManager;
 
-//    // ReSharper disable once ConvertToPrimaryConstructor
-//    public Processes(ILogger<Processes> logger)
-//    {
-//        _logger = logger;
-//    }
+    // ReSharper disable once ConvertToPrimaryConstructor
+    public Processes(ILogger<Processes> logger)
+    {
+        _logger = logger;
+    }
 
-//    public bool IsBusy()
-//    {
-//        return _processManager != null && _processManager.IsBusy();
-//    }
+    public bool IsBusy()
+    {
+        return _processManager != null && _processManager.IsBusy();
+    }
 
-//    public async Task WaitForFinishAll()
-//    {
-//        if (_processManager != null)
-//            await _processManager.WaitForFinishAll();
-//    }
+    public async Task WaitForFinishAll()
+    {
+        if (_processManager != null)
+            await _processManager.WaitForFinishAll();
+    }
 
-//    public void CancelProcesses()
-//    {
-//        _processManager?.CancelProcesses();
-//        ClearProcessManager();
-//    }
+    public void CancelProcesses()
+    {
+        _processManager?.CancelProcesses();
+        ClearProcessManager();
+    }
 
-//    public ProcessManager GetNewProcessManager()
-//    {
-//        _processManager = new ProcessManager(_logger);
-//        return _processManager;
-//    }
+    public ProcessManager GetNewProcessManager()
+    {
+        // ReSharper disable once DisposableConstructor
+        _processManager = new ProcessManager(_logger);
+        return _processManager;
+    }
 
-//    public void ClearProcessManager()
-//    {
-//        _processManager?.Dispose();
-//        _processManager = null;
-//    }
-//}
+    public void ClearProcessManager()
+    {
+        _processManager?.Dispose();
+        _processManager = null;
+    }
+}
