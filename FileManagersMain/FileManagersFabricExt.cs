5f18ce559dda2eec137d23672fa6e852c695f755
merabza <merabza@gmail.com>
2024-04-18 22:29:08 +0400

    202404182229

--- FileManagersMain/FileManagersFabricExt.cs
+++ FileManagersMain/FileManagersFabricExt.cs
@@ -15,8 +15,7 @@ public static class FileManagersFabricExt
     }
 
     public static async Task<(FileStorageData?, FileManager?)> CreateFileStorageAndFileManager(bool useConsole,
-        ILogger logger,
-        string localPatch, string? fileStorageName, FileStorages fileStorages,
+        ILogger logger, string localPatch, string? fileStorageName, FileStorages fileStorages,
         IMessagesDataManager? messagesDataManager, string? userName, CancellationToken cancellationToken)
     {
         FileStorageData? fileStorageData = null;
