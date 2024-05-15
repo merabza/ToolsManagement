95ddfd7a7485a33ff9714be423ae0cedbe735285
merabza <merabza@gmail.com>
2024-05-15 13:10:23 +0400

    202405151309

--- LibToolActions/ToolAction.cs
+++ LibToolActions/ToolAction.cs
@@ -2,6 +2,7 @@
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
+using SignalRContracts;
 using SystemToolsShared;
 
 // ReSharper disable ConvertToPrimaryConstructor
