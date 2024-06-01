bf801230b1fceaa1ca60681c778df0e47722380f
merabza <merabza@gmail.com>
2024-06-01 13:50:20 +0400

    202406011349

--- LibToolActions/BackgroundTasks/ProcessesToolAction.cs
+++ LibToolActions/BackgroundTasks/ProcessesToolAction.cs
@@ -1,7 +1,7 @@
 ﻿using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
-using SignalRContracts;
+using SystemToolsShared;
 
 namespace LibToolActions.BackgroundTasks;
 
--- LibToolActions/ToolAction.cs
+++ LibToolActions/ToolAction.cs
@@ -2,7 +2,6 @@
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
-using SignalRContracts;
 using SystemToolsShared;
 
 namespace LibToolActions;
