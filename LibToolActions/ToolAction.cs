3927d33f0566d8969e99ca24d1c62dc42256480a
merabza <merabza@gmail.com>
2024-11-12 21:56:35 +0400

    202411122155

--- LibToolActions/ToolAction.cs
+++ LibToolActions/ToolAction.cs
@@ -17,8 +17,7 @@ public /*open*/ class ToolAction : MessageLogger
 
 
     protected ToolAction(ILogger? logger, string actionName, IMessagesDataManager? messagesDataManager,
-        string? userName,
-        bool useConsole = false) : base(logger, messagesDataManager, userName, useConsole)
+        string? userName, bool useConsole = false) : base(logger, messagesDataManager, userName, useConsole)
     {
         Logger = logger;
         ToolActionName = actionName;
